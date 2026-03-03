75d0d1941d2f070d8ba11823a0f3e1064b94e43d
Jiayi Li <mcaljy@gmail.com>
2026-03-03 12:52:20 +0100

    Depuración: Cambios en HeroSection
    Se ha actualizado style.css, escribiendo las clases de HeroSection en style.css para una mejor lectura y organización en un futuro.

--- src/components/sections/HeroSection.vue
+++ src/components/sections/HeroSection.vue
@@ -1,35 +1,27 @@
 <template>
-    <section
-        class="min-h-screen flex items-center justify-center bg-background text-foreground px-6 transition-colors duration-300"
-    >
-        <div
-            class="max-w-6xl w-full grid grid-cols-1 md:grid-cols-2 gap-12 items-center"
-        >
+    <section class="min-h-screen flex items-center justify-center">
+        <div class="section-container w-full grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+            
             <div class="flex justify-center">
-                <div
-                    class="w-72 md:w-96 aspect-square bg-foreground rounded-md shadow-lg transition-colors duration-300"
-                ></div>
+                <div class="w-72 md:w-96 aspect-square bg-foreground rounded-md shadow-lg"></div>
             </div>
 
             <div class="flex flex-col justify-center items-center md:items-start text-center md:text-left space-y-6">
-                <h1 class="text-4xl md:text-5xl font-bold">
+                <h1 class="hero-title">
                     ¡Hola! Soy Mencía
                 </h1>
 
-                <p class="text-lg leading-relaxed max-w-md mx-auto md:mx-0">
+                <p class="hero-text">
                     Soy una estudiante de Diseño y Tecnologías Creativas. <br> 
-
                     Mis intereses abarcan desde lo gráfico y lo ilustrado hasta lo editorial, interactivo y audiovisual.
                 </p>
-                <p class="text-lg leading-relaxed max-w-md mx-auto md:mx-0">
+
+                <p class="hero-text">
                     Disfruto moviéndome entre disciplinas y entendiendo el diseño como un territorio amplio donde creatividad y técnica se complementan.
                 </p>
 
                 <div class="w-full flex justify-center md:justify-start">
-                    <button
-                        @click="goToAbout"
-                        class="bg-foreground text-background px-6 py-3 rounded-md font-semibold hover:opacity-90 transition-all cursor-pointer active:scale-95"
-                    >
+                    <button @click="goToAbout" class="btn-primary">
                         ¡Cuéntame más!
                     </button>
                 </div>
@@ -40,6 +32,5 @@
 
 <script setup lang="ts">
 import { useNavigation } from "@/composables/useNavigation"
-
 const { goToAbout } = useNavigation()
 </script>
